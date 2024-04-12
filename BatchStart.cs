4d5aa5437430adaaa082f9aeaf57f1ba2442438a
Shuichi Tsukamoto <shtsukam@microsoft.com>
2024-04-12 12:31:01 +0900

    update.

--- BatchStart.cs
+++ BatchStart.cs
@@ -2,6 +2,7 @@ using System.Net;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace functions_azure_batch_if
 {
@@ -9,34 +10,42 @@ namespace functions_azure_batch_if
     {
         private readonly ILogger _logger;
 
+        static BatchStart()
+        {
+            //var root = Directory.GetCurrentDirectory();
+            //var dotenv = Path.Combine(root, ".env");
+            //DotNetEnv.Env.Load(dotenv);
+            DotNetEnv.Env.Load();
+        }
         public BatchStart(ILoggerFactory loggerFactory)
         {
             _logger = loggerFactory.CreateLogger<BatchStart>();
         }
 
         [Function("BatchStart")]
-        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+        async public Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
 
             var response = req.CreateResponse(HttpStatusCode.OK);
-            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+            response.Headers.Add("Content-Type", "application/json; charset=utf-8");
             //AzureBatchRequest(string poolId, string jobId, string taskId, string commandLine, string batchAccountUrl, string batchAccountName, string batchAccountKey, TimeSpan timeout)
             var timeStamp = DateTime.Now.ToString("yyyyMMddhhmmssSSS");
             var ba = new AzureBatch(
                 new AzureBatchRequest(
                     poolId: DotNetEnv.Env.GetString("BATCH_POOL_ID"),
-                    jobId: $"{DotNetEnv.Env.GetString("BATCH_JOB_ID")}_{timeStamp}",
-                    taskId: $"taskid_{timeStamp}",
-                    commandLine: "",
-                    batchAccountUrl: DotNetEnv.Env.GetString("BATCH_JOB_ID"),
-                    batchAccountName: DotNetEnv.Env.GetString("BATCH_JOB_ID"),
-                    batchAccountKey: DotNetEnv.Env.GetString("BATCH_JOB_ID"),
+                    jobId: $"{DotNetEnv.Env.GetString("BATCH_JOB_ID")}{timeStamp}",
+                    taskId: $"taskid{timeStamp}",
+                    commandLine: $"powershell -Command \"echo 'Hello World! {timeStamp}'\"",
+                    batchAccountUrl: DotNetEnv.Env.GetString("BATCH_ACCOUNT_URL"),
+                    batchAccountName: DotNetEnv.Env.GetString("BATCH_ACCOUNT_NAME"),
+                    batchAccountKey: DotNetEnv.Env.GetString("BATCH_ACCOUNT_KEY"),
                     timeout: TimeSpan.FromMinutes(30)
                 ),
                 logger: this._logger);
-            response.WriteString("Welcome to Azure Functions!");
-
+            var r = ba.doSubmit();
+            var jsonToReturn = JsonConvert.SerializeObject(r);
+            await response.WriteStringAsync(jsonToReturn);
             return response;
         }
     }
